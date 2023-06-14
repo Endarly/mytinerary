b7e23bcb26af1e0426ab6cb588c586822d8dc6b9
endarly <endarly@gmail.com>
2023-06-13 22:23:39 -0300

    add footer

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Team members: America, Nazareth y Endarly
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,9 +1,180 @@
- function Footer(){
-     return(
-         <div>
-             <div className="footer"></div>
-         </div>
-     )
- }
+import React from 'react';
+import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon, MDBBtn } from 'mdb-react-ui-kit';
+import '../style/footer.css';
+export default function Footer() {
+  return (
+    <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
+      <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
+        <div className='me-5 d-none d-lg-block'>
+          <span>Get connected with us on social networks:</span>
+        </div>
 
-export default Footer
+        <section className='mb-4'>
+          <MDBBtn
+            floating
+            className='m-1'
+            style={{ backgroundColor: '#3b5998' }}
+            href='#!'
+            role='button'
+          >
+            <MDBIcon fab icon='facebook-f' />
+          </MDBBtn>
+
+          <MDBBtn
+            floating
+            className='m-1'
+            style={{ backgroundColor: '#55acee' }}
+            href='#!'
+            role='button'
+          >
+            <MDBIcon fab icon='twitter' />
+          </MDBBtn>
+
+          <MDBBtn
+            floating
+            className='m-1'
+            style={{ backgroundColor: '#dd4b39' }}
+            href='#!'
+            role='button'
+          >
+            <MDBIcon fab icon='google' />
+          </MDBBtn>
+          <MDBBtn
+            floating
+            className='m-1'
+            style={{ backgroundColor: '#ac2bac' }}
+            href='#!'
+            role='button'
+          >
+            <MDBIcon fab icon='instagram' />
+          </MDBBtn>
+
+          <MDBBtn
+            floating
+            className='m-1'
+            style={{ backgroundColor: '#0082ca' }}
+            href='#!'
+            role='button'
+          >
+            <MDBIcon fab icon='linkedin-in' />
+          </MDBBtn>
+
+          <MDBBtn
+            floating
+            className='m-1'
+            style={{ backgroundColor: '#333333' }}
+            href='#!'
+            role='button'
+          >
+            <MDBIcon fab icon='github' />
+          </MDBBtn>
+        </section>
+      </section>
+
+      <section className=''>
+        <MDBContainer className='text-center text-md-start mt-5'>
+          <MDBRow className='mt-3'>
+            <MDBCol md="3" lg="4" xl="3" className='mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>
+                <MDBIcon icon="gem" className="me-3" />
+                MyTinerary
+              </h6>
+              <p>
+                Mytinerary is the travel portal that connects you with the world. We offer more than just packages; rather, we provide packaged experiences waiting to be explored. Each one is unique in its own way, giving you the opportunity to discover incredible destinations across all continents while enjoying top-notch services. It's time to pack your bags!.
+              </p>
+            </MDBCol>
+
+            <MDBCol md="2" lg="2" xl="2" className='mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Products</h6>
+              <p>
+                <a href='#!' className='text-reset'>
+                  About Us
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Contact
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Frequently Asked Questions
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Privacy Policy
+                </a>
+              </p>
+            </MDBCol>
+
+            <MDBCol md="3" lg="2" xl="2" className='mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Travel</h6>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Africa
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Antarctica
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Asia
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Europe
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  North America
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  South America
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Australia
+                </a>
+              </p>
+
+            </MDBCol>
+
+            <MDBCol md="4" lg="3" xl="3" className='mx-auto mb-md-0 mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
+              <p>
+                <MDBIcon icon="home" className="me-2" />
+                Buenos Aires, CABA 10012
+              </p>
+              <p>
+                <MDBIcon icon="envelope" className="me-3" />
+                info@example.com
+              </p>
+              <p>
+                <MDBIcon icon="phone" className="me-3" /> + 01 234 567 88
+              </p>
+              <p>
+                <MDBIcon icon="print" className="me-3" /> + 01 234 567 89
+              </p>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </section>
+
+      <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
+        © 2021 Copyright:
+        <a className='text-reset fw-bold' href='https://mdbootstrap.com/'>
+          Mytinerary.com
+        </a>
+      </div>
+    </MDBFooter>
+  );
+}
