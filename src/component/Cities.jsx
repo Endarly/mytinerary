27c958593f6aa4c9a038c22869e4be4510b422c7
nazarethgt <gtnazareth@gmail.com>
2023-07-04 21:18:06 -0300

    ignorar commit

--- src/component/Cities.jsx
+++ src/component/Cities.jsx
@@ -1,49 +1,19 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import CityCard from './CityCard';
-// import { Link as LinkRouter } from "react-router-dom";
+import React from 'react';
+import underConstructionImage from '../assests/jackhanmer-construction-worker.gif';
+import '../style/cities.css';
 
 function Cities() {
-  const [cities, setCities] = useState([]);
-
-  async function getData() {
-    try {
-      let citiesDB = await axios.get("http://localhost:5000/api/cities");
-      setCities(citiesDB.data.response);
-    } catch (error) {
-      console.error("Error fetching cities:", error);
-    }
-  }
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-      // const handleToggle = () => {
-    //     setIsExpanded(!setExpanded)
-    // };
   return (
-    <div>
-      {cities && cities.length > 0 ? (
-        <ul>
-          {cities.map((city, index) => (
-              // <LinkRouter key={index} to={'/CityDetails/'+city.id}>
-            <li >
-              {/* <Search city={city} /> */}
-              <CityCard city={city} />
-            </li>
-            // </LinkRouter>
-
-          ))}
-        </ul>
-      ) : (
-        <h1>Loading</h1>
-      )}
+    <div className='containerCities'>
+      <h3>CITIES IS UNDER CONSTRUCTION</h3>
+      <img
+        src={underConstructionImage}
+        alt="Under Construction"
+        style={{ width: '40%', height: '90%' }}
+      />
     </div>
   );
 }
 
 export default Cities;
-
-
 
