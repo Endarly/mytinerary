9b3d4d7f2f62bd5d26cd2b2eeffaaf65d926d8f2
endarly <endarly@gmail.com>
2023-07-05 20:51:51 -0300

    Funcionalidad Serch

--- src/component/Cities.jsx
+++ src/component/Cities.jsx
@@ -3,73 +3,70 @@ import axios from 'axios';
 import CityCard from './CityCard';
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import "../style/Search.css"
+import ClipLoader from "react-spinners/ClipLoader";
+import "../style/Search.css";
 import '../style/cities.css';
-// import { Link as LinkRouter } from "react-router-dom";
 
 function Cities() {
   const [cities, setCities] = useState([]);
-  const [cityList, setCityList] = useState([]); // datos estaticos
-  const [search, setSearch] = useState(""); // busqueda
-
-  async function getData() {
-    try {
-      let citiesDB = await axios.get("https://endarly-api-cities-crud.onrender.com/api/cities");
-      setCities(citiesDB.data.response);
-    } catch (error) {
-      console.error("Error fetching cities:", error);
-    }
-  }
+  const [filteredCities, setFilteredCities] = useState([]);
+  const [search, setSearch] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     getData();
   }, []);
 
-  // const handleToggle = () => {
-  //     setIsExpanded(!setExpanded)
-  // };
+  async function getData() {
+    try {
+      let citiesDB = await axios.get("https://endarly-api-cities-crud.onrender.com/api/cities");
+      setCities(citiesDB.data.response);
+      setFilteredCities(citiesDB.data.response);
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Error fetching cities:", error);
+    }
+  }
+
   const handleChange = (e) => {
-    setSearch(e.target.value)
-    filtrar(e.target.value)
-    console.log(e.target.value)
-}
-
-const filtrar = (termoBusca) => {
-    var searchResults = cityList.filter((elemento) => {
-        if (elemento.nome.toString().toLowerCase().includes(termoBusca.toLowerCase())) {
-            return elemento
-        }
-    })
-    setCityList(searchResults)
-}
-
+    const searchTerm = e.target.value;
+    setSearch(searchTerm);
+    filtrar(searchTerm);
+  };
+  
+  const filtrar = (termoBusca) => {
+    const searchResults = cities.filter((elemento) => {
+      const cityName = elemento.name?.toLowerCase() || '';
+      return cityName.includes(termoBusca.toLowerCase());
+    });
+    setFilteredCities(searchResults);
+    console.log(searchResults);
+  };
 
   return (
     <>
       <div className="Search">
-            <input value={search} onChange={handleChange} type="text" />
-            <button className="">
-                <FontAwesomeIcon icon={faSearch} />
-            </button>
-        </div>
+        <input value={search} onChange={handleChange} type="text" />
+        <button className="">
+          <FontAwesomeIcon icon={faSearch} />
+        </button>
+      </div>
 
       <div>
-        {cities && cities.length > 0 ? (
+        {isLoading ? (
+          <ClipLoader color="rgba(255, 68, 0, 0.418)" loading={isLoading} size={30} css={{ display: "block", margin: "0 auto" }} />
+        ) : filteredCities.length > 0 ? (
           <div className="cityCards-container">
-            {cities.map((city, index) => (
-              // <LinkRouter key={index} to={'/CityDetails/'+city.id}>
+            {filteredCities.map((city, index) => (
               <div key={index}>
                 <CityCard city={city} />
               </div>
-              // </LinkRouter>
-
             ))}
-         </div>
+          </div>
         ) : (
-          <h4>Loading</h4>
+          <h4>No cities found</h4>
         )}
       </div>
-
     </>
   );
 }
