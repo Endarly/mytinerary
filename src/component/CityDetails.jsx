a32841afc3352bebce8b39f550e6fa15b2112764
endarly <endarly@gmail.com>
2023-07-10 21:25:22 -0300

    agregar Detalles a CityDetails

--- src/component/CityDetails.jsx
+++ src/component/CityDetails.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import underConstructionImage from '../assests/jackhanmer-construction-worker.gif';
 import '../style/cities.css';
 import { useParams } from 'react-router-dom';
 
@@ -12,7 +11,8 @@ function CityDetails() {
     async function getCity(id) {
         try {
             const response = await axios.get(`https://endarly-api-cities-crud.onrender.com/api/cities/${id}`);
-            setCity(response.data);
+            setCity(response.data.response);
+            console.log(response.data.response)
         } catch (error) {
             console.error("Error fetching city:", error);
         }
@@ -23,27 +23,31 @@ function CityDetails() {
     }, [id]);
 
     return (
-        <div>
-            {city ? (
-                <div className='containerCities'>
-                    <h1>CITIES IS UNDER CONSTRUCTION</h1>
-                    <h3>CITY NAME: {city.name}</h3>
-                    <p>Country: {city.country}</p>
-                    <p>Description: {city.description}</p>
-                    {/* Other city details */}
-                    {/* className='title'>{city[0].className} */}
-                    {/* siempre buscar la posicion 0 [0] */}
-                    <img
-                        src={underConstructionImage}
-                        alt="Under Construction"
-                        style={{ width: '40%', height: '90%' }}
-                    />
+        <div className='containerCities'>
+          {city ? (
+            <>
+              <img src={city.image} alt={city.name} />
+              <div className='cityDetails'>
+                <div className='name-avatar-wrapper'>
+                  <h3>{city.name}</h3>
+                  <img src={city.avatarCountry} alt={city.country} className='avatar' />
                 </div>
-            ) : (
-                <h1>Loading</h1>
-            )}
+                <p className="country">Country: {city.country}</p>
+                <p className="continent">Continent: {city.continent}</p>
+                <p className="description">Description: {city.description}</p>
+                <div className="itinerary-buttons">
+                  <button className="itineraryLink ">Itinerary 1</button>
+                  <button className="itineraryLink">Itinerary 2</button>
+                  <button className="itineraryLink">Itinerary 3</button>
+                </div>
+              </div>
+            </>
+          ) : (
+            <h1>Loading</h1>
+          )}
         </div>
-    );
+      );
+      
 }
 
 export default CityDetails;
