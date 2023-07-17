9269f65879fb921025762f0b7ae584c8441a311e
endarly <endarly@gmail.com>
2023-07-17 15:21:40 -0300

    agregar scrollToTop

--- src/component/Cities.jsx
+++ src/component/Cities.jsx
@@ -1,5 +1,5 @@
 import "../style/App.css";
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import CityCard from "./CityCard";
 import Search from "./SearchNew";
 import { useDispatch, useSelector } from "react-redux";
@@ -16,23 +16,47 @@ function Cities() {
     dispatch(citiesActions.getCities());
   }, [dispatch]);
 
+  const [showScrollButton, setShowScrollButton] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 200) {
+        setShowScrollButton(true);
+      } else {
+        setShowScrollButton(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <>
-    <div className="mainContainer">
-      <Search cities={cities} />
-      <div className="cityCards-container">
-        
-        {cities && cities.length > 0 ? (
-          searchResult.map((city, index) => {
-            return <CityCard key={index} city={city} />;
-          })
-        ) : (
-          <h1>....LOADING</h1>
+      <div className="mainContainer">
+        <Search cities={cities} />
+        <div className="cityCards-container">
+          {cities && cities.length > 0 ? (
+            searchResult.map((city, index) => {
+              return <CityCard key={index} city={city} />;
+            })
+          ) : (
+            <h1>....LOADING</h1>
+          )}
+        </div>
+        {showScrollButton && (
+          <button className="scroll-button" onClick={scrollToTop}>
+            ↑
+          </button>
         )}
       </div>
-    </div>
     </>
-
   );
 }
 
