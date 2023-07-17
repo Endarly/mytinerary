3f6670716cfbe76f3a2d062240be9b7ad78681fd
endarly <endarly@gmail.com>
2023-07-17 15:39:32 -0300

    deshabilitar funcionalidades de las cards

--- src/component/CityCard.jsx
+++ src/component/CityCard.jsx
@@ -5,15 +5,15 @@ import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
+// import Collapse from '@mui/material/Collapse';
 import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
+// import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+// import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+// import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { Link } from 'react-router-dom';
 import '../style/cities.css';
 
@@ -46,7 +46,7 @@ function CityCard({ city }) {
                     }
                     action={
                         <IconButton aria-label="settings">
-                            <MoreVertIcon />
+                            {/* <MoreVertIcon /> */}
                         </IconButton>
                     }
                     title={city.name}
@@ -70,7 +70,7 @@ function CityCard({ city }) {
                 </CardContent>
                 <CardActions disableSpacing>
                     <IconButton aria-label="add to favorites">
-                        <FavoriteIcon />
+                        {/* <FavoriteIcon /> */}
                     </IconButton>
                     <IconButton aria-label="share">
                         <ShareIcon />
@@ -81,18 +81,18 @@ function CityCard({ city }) {
                         aria-expanded={expanded}
                         aria-label="show more"
                     >
-                        <ExpandMoreIcon />
+                        {/* <ExpandMoreIcon /> */}
                     </ExpandMore>
                     <Link to={`/CityDetails/${city._id}`} className="details-link">
                         show more
                     </Link>
                 </CardActions>
-                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                {/* <Collapse in={expanded} timeout="auto" unmountOnExit>
                     <CardContent>
                         <Typography paragraph>Description:</Typography>
                         <Typography paragraph>{city.description}</Typography>
                     </CardContent>
-                </Collapse>
+                </Collapse> */}
             </Card>
         </div>
     );
