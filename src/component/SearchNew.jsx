81871b01f3e9790a9a38685cebc880bd86f1631b
nazarethgt <gtnazareth@gmail.com>
2023-07-25 22:21:02 -0300

    ajustes en css del componente search

--- src/component/SearchNew.jsx
+++ src/component/SearchNew.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react"
 import { useDispatch } from "react-redux"
 import citiesActions from "../redux/action/citiesActions"
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import "../style/Search.css"
 
 function Search(props) {
@@ -17,10 +19,13 @@ function Search(props) {
   }
 
   return (
+    <>
     <div className="searchContainer">
-      <input className="inputSearch" placeholder="Enter your search" onChange={(e) => { filter(e) }} />
-      {noResults && <p className="noResultsMessage">No cities found</p>}
+      <i><FontAwesomeIcon icon={faSearch}/></i>
+      <input type="text" placeholder="Enter your search" onChange={(e) => { filter(e) }}/>
     </div>
+    <div className="noResultsMessage">{noResults && <p>No cities found</p>}</div>
+    </>
   )
 }
 
